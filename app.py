3df107b16922f22c30d774f84bf36ee4058cc896
mmcdonald3n <mmcdonald3@neogen.com>
2025-09-02 15:20:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ Here is the job description:
         max_tokens=1200,
         temperature=0.7
     )
+
     return response.choices[0].message['content']
 
 # --- Convert advert text to .docx ---
